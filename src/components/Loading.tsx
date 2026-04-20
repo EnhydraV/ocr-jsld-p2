c759bf1198ca9c71b2d5ee590fb5d9b32d92574d
Vincent Vanwaelscappel <vincent@cubedesigners.com>
2026-04-20 19:35:58 +0200

    ajout d'un spinner sur l'écran de chargement message d'erreur plus clair sur la 404

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,6 +1,15 @@
+import {MoonLoader} from "react-spinners";
+
 const Loading = () => {
     return <div
-        className="absolute top-0 left-0 w-full h-full bg-gray-900 text-white flex items-center justify-center cursor-wait text-2xl">Chargement en cours</div>
+        className="absolute top-0 left-0 w-full h-full bg-gray-900 text-white flex items-center justify-center cursor-wait text-2xl">
+        <div>
+            <div className="flex justify-center mb-8">
+                <MoonLoader color="#ffffff"/>
+            </div>
+            <p>Chargement en cours</p>
+        </div>
+    </div>
 }
 
 export default Loading
