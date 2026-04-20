4807b1715f255ac88ae7dac75f14c2c4aaaa21c4
Vincent Vanwaelscappel <vincent@cubedesigners.com>
2026-04-20 21:11:04 +0200

    styles responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -16,12 +16,21 @@ export const Header = ({title, presentation, cards}: {
         </div>
     }
 
+    const colsMap: Record<number, string> = {
+        1: 'md:grid-cols-1',
+        2: 'md:grid-cols-2',
+        3: 'md:grid-cols-3',
+        4: 'md:grid-cols-4',
+    };
+    const gridCols = colsMap[Math.min(4, cards.length)] ?? 'grid-cols-4';
+
+
     return <header>
         <h1 className="text-4xl font-bold mb-8">
             {title}
         </h1>
         {presentationDiv}
-        <div className="mb-2">
+        <div className={`grid gap-4 mb-4 ${gridCols} grid-cols-1`}>
             {cards.map((card, index) => (
                 <IndicatorCard key={index} label={card.label} value={card.value} color={card.color}/>
             ))}
--- src/components/IndicatorCard.tsx
+++ src/components/IndicatorCard.tsx
@@ -1,9 +1,9 @@
-const IndicatorCard = ({label, value, color}:{label:string, value: number|string, color:string}    ) =>
-    <div key={label} className="bg-gray-800 p-6 rounded-lg shadow-lg text-center mb-2">
+const IndicatorCard = ({label, value, color}: { label: string, value: number | string, color: string }) => {
+    return <div key={label} className="bg-gray-800 p-6 rounded-lg shadow-lg text-center">
         <h3 className="text-xl font-semibold mb-2">{label}</h3>
         <p className={`text-4xl font-bold ${color}`}>
             {value}
         </p>
     </div>
-
+}
 export default IndicatorCard
